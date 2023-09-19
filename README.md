0577c3a06b546d0103a0a258f103ac9f947bca8e
Sujal <sranjan0208@gmail.com>
2023-09-20 02:11:54 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Create Clean React CLI
+
+A CLI tool for creating and cleaning up React projects.
+
+## Installation
+
+You can install this package globally using npm:
+
+```bash
+npm install -g create-clean-react
+```
+
+## Usage
+
+To create a new clean React app, run the following command:
+
+```bash
+create-clean-react
+```
+
+Follow the prompts to enter the name of your app.
+
+## What does it do?
+
+- Initializes a new React app using Vite.
+- Removes unnecessary files and default content.
+- Sets up Tailwind CSS for styling.
+
+## Features
+
+- Automatically configures your project for a clean start.
+- Provides a simple and quick way to kickstart your React projects.
+
+## Dependencies
+
+This CLI relies on the following dependencies:
+
+- `shelljs`: For executing shell commands.
+- `fs`: For interacting with the file system.
+- `inquirer`: For interactive command-line prompts.
+- `chalk`: For adding color to console output.
+
+## Contributing
+
+If you'd like to contribute or report an issue, please check out the [GitHub repository](https://github.com/Sranjan0208/create-clean-react).
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
---
+++ cli.js
@@ -0,0 +1,123 @@
+#!/usr/bin/env node
+
+import pkg from "shelljs";
+import { existsSync, unlinkSync, writeFileSync } from "fs";
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const { exec } = pkg;
+
+function createReactApp(appName) {
+  exec(`npm create vite@latest ${appName} -- --template react`);
+  console.log(chalk.green(`✓ Created React app: ${appName}`));
+}
+
+function cleanReactApp(appName) {
+  const appDirectory = `${process.cwd()}/${appName}`;
+
+  console.log(chalk.green(`✓ Changed directory to ${appDirectory}`));
+
+  // List of files/folders to remove
+  const filesToRemove = ["src/assets/react.svg", "public/vite.svg"];
+
+  filesToRemove.forEach((file) => {
+    const filePath = `${appDirectory}/${file}`;
+    if (existsSync(filePath)) {
+      unlinkSync(filePath);
+      console.log(chalk.green(`✓ Removed ${filePath}`));
+    }
+  });
+
+  // Modify src/App.jsx (or .js) to remove default content
+  const appJsPath = `${appDirectory}/src/App.jsx`; // Adjust for JSX or JS
+  if (existsSync(appJsPath)) {
+    writeFileSync(
+      appJsPath,
+      'import React from "react";\n\nconst App = () => {\n  return <div className="text-xl font-bold underline">I am the Honored One!!</div>;\n}\n\nexport default App;\n'
+    );
+    console.log(chalk.green("✓ Modified: src/App.jsx"));
+  } else {
+    console.log(
+      chalk.yellow(
+        "Warning: src/App.jsx not found. Please make sure it exists."
+      )
+    );
+  }
+
+  // Modify src/App.css to remove default content
+  const appCssPath = `${appDirectory}/src/App.css`;
+  if (existsSync(appCssPath)) {
+    writeFileSync(appCssPath, "");
+    console.log(chalk.green("✓ Cleared: src/App.css"));
+  } else {
+    console.log(
+      chalk.yellow(
+        "Warning: src/App.css not found. Please make sure it exists."
+      )
+    );
+  }
+
+  // Modify src/index.css to remove default content
+  const indexCssPath = `${appDirectory}/src/index.css`;
+  if (existsSync(indexCssPath)) {
+    writeFileSync(
+      indexCssPath,
+      `@tailwind base;\n@tailwind components;\n@tailwind utilities;\n`
+    );
+    console.log(chalk.green("✓ Modified: src/index.css"));
+  } else {
+    console.log(
+      chalk.yellow(
+        "Warning: src/index.css not found. Please make sure it exists."
+      )
+    );
+  }
+
+  // install tailwind css
+  console.log(chalk.green("Installing and Initializing Tailwind CSS"));
+  exec(`cd ${appDirectory} && npm install -D tailwindcss postcss autoprefixer`);
+
+  // Initialize Tailwind CSS
+  exec(`cd ${appDirectory} && npx tailwindcss init -p`);
+  console.log(chalk.green("✓ Installed and Initialized Tailwind CSS"));
+
+  // Modify tailwind.config.js
+  const tailwindConfigPath = `${appDirectory}/tailwind.config.js`;
+  if (existsSync(tailwindConfigPath)) {
+    writeFileSync(
+      tailwindConfigPath,
+      `/** @type {import('tailwindcss').Config} */\n export default { \n content: ["./index.html","./src/**/*.{js,ts,jsx,tsx}",],\n theme:{\n extend: {},\n },\n plugins: [],\n};`
+    );
+    console.log(chalk.green("✓ Modified: tailwind.config.js"));
+  } else {
+    console.log(
+      chalk.yellow(
+        "Warning: tailwind.config.js not found. Please make sure it exists."
+      )
+    );
+  }
+
+  console.log(chalk.green("✓ Running vite"));
+  exec(`cd ${appDirectory} && npm install && npm run dev`);
+}
+
+function init() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "appName",
+        message: "Enter the name of your React app:",
+      },
+    ])
+    .then((answers) => {
+      const { appName } = answers;
+      createReactApp(appName);
+      cleanReactApp(appName);
+      console.log(
+        chalk.green(`Successfully created a clean React app: ${appName}`)
+      );
+    });
+}
+
+init();
